aa6e972b33ab2c879cda4a61eab55ba1f1b31536
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-10-13 14:50:57 -0400

    start expecting range objects for ip's and "auth" objects for auth

--- src/rho/ssh_jobs.py
+++ src/rho/ssh_jobs.py
@@ -10,18 +10,44 @@ import sys
 import time
 import StringIO
 
+class Auth():
+    def __init__(self, name=None, type=None, username=None, password=None):
+        self.name = name
+        self.type = type
+        self.username = username
+        self.password = None
+
+class SshAuth(Auth):
+    def __init__(self, name=None, type=None, username=None, password=None):
+        self.name = name
+        self.type = "ssh"
+        self.username = username
+        self.password = None
+
+
 
 class SshJob():
     def __init__(self, ip=None, cmd=None, auth=None):
         self.ip = ip
         self.cmd = cmd
+        self.auth = auth
+        self.ssh_cmd = ["ssh", "%s@%s" % (self.auth.username, self.ip), "%s" % (self.cmd)]
         self.output = None
+        self.returncode = None
+
+        #self.config = config.Config()['config']
+        #self.auth = self.config.credentials['bobslogin']
         # what is auth? undetermined yet
-        self.auth = None
+#        self.auth = self.config['
 
     def run(self):
-        (stdout, stderr) = subprocess.Popen(self.cmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
+#        print self.ssh_cmd
+        p = subprocess.Popen(self.ssh_cmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        (stdout, stderr) = p.communicate()
+        retcode = p.returncode
+#        print stdout, stderr, retcode
         self.output = stdout
+        self.returncode = retcode
 
 class SshJobs():
     def __init__(self, cmdSrc=None):
@@ -41,7 +67,7 @@ class SshJobs():
             job.run()
             if callback:
                 callback([job])
-            print job.output
+#            print job.output
             self.jobs.append(job)
 
     def add_cmd_to_run(self, job):
@@ -51,14 +77,21 @@ class SshJobs():
         pass
 
 
+def example_callback(resultlist=[]):
+    for result in resultlist:
+        print "%s: %s" % (result.ip, result.output)
+
 if __name__ == "__main__":  
     ssh_cmds = []
-    for i in range(1,5):
-        ip = "192.168.1.%s" % i
-        ssh_cmds.append(SshJob(ip=ip, cmd=["ssh", "adrian@alikins.usersys.redhat.com", "echo", ip]))
+
+    auth = SshAuth(name="adrian", username="adrian")
+
+    ip_range = ["alikins.usersys.redhat.com", "badhost.example.com"]
+    for ip in ip_range:
+        ssh_cmds.append(SshJob(ip=ip, cmd="echo foo bar %s" % ip, auth=auth ))
         
     jobs = SshJobs()
     jobs.cmds_to_run = ssh_cmds
-    jobs.run_cmds()
+    jobs.run_cmds(callback=callback)
 
 #jobs.read_jobs()
