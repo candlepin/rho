1467a430b3e3b7e1cf2130f8e5fa3b88fb817a4f
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-10-13 15:43:08 -0400

    add __test__=False so nosetest ignores this for now
    import a "local_auth" for storing auth info. Do not
    check in local_auth

--- test/ssh_jobs_test.py
+++ test/ssh_jobs_test.py
@@ -6,15 +6,26 @@ import unittest
 from rho import config
 from rho import ssh_jobs
 
+__test__ = False
+
 hostname = "alikins.usersys.redhat.com"
 bad_hostname = "thisshouldneverexist.usersys.redhat.com"
 filtered_hostname = ""
 user = ""
 
+# haha, no password, only works current if ssh-agent is running, and
+# hopefully, if you are me
+
+
 auth_good =  ssh_jobs.SshAuth(name="adrian", username="adrian")
 auth_no_user = ssh_jobs.SshAuth(name="badadrian", username="badadrian")
 auth_bad_password = ssh_jobs.SshAuth(name="adrian", username="adrian", password="wrong")
 
+# local_auth should redefine the above if need be
+try:
+    from local_auth import *
+except ImportError:
+    pass
 # this api is going to change...
 class _TestSshJobs(unittest.TestCase):
     auth = None
@@ -43,3 +54,8 @@ class TestSshJobsWorks(_TestSshJobs):
 class TestSshJobsNoHost(_TestSshJobs):
     auth = auth_good
     ip = bad_hostname
+
+class TestSshJobsF11(_TestSshJobs):
+    auth = auth_test
+    ip = "f11-virt-1.usersys.redhat.com"
+
