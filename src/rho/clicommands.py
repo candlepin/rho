4d1a0d607c9b5bad745ca1b41bbdc51c2ac22f5e
jesus m. rodriguez <jesusr@redhat.com>
2009-10-15 15:48:02 -0400

    writing auth to config file, including 2 util scripts

--- src/rho/clicommands.py
+++ src/rho/clicommands.py
@@ -23,7 +23,8 @@ _ = t.ugettext
 from optparse import OptionParser
 from getpass import getpass
 
-from rho.config import *
+from rho import config
+from rho import crypto
 
 RHO_PASSPHRASE = "RHO_PASSPHRASE"
 
@@ -46,7 +47,7 @@ class CliCommand(object):
         self.parser.add_option("--debug", dest="debug",
                 help=_("enable debug output"))
         self.parser.add_option("--config", dest="config",
-                help=_("config file name"))
+                default="~/.rho.conf", help=_("config file name"))
 
     def _validate_options(self):
         """ 
@@ -56,6 +57,11 @@ class CliCommand(object):
 
     def _do_command(self):
         pass
+
+    def _read_config(self, filename, passphrase):
+        confstr = crypto.read_file(filename, passphrase)
+        print(confstr)
+        return config.ConfigBuilder().build_config(confstr)
 
     def main(self):
         (self.options, self.args) = self.parser.parse_args()
@@ -70,6 +76,9 @@ class CliCommand(object):
             self.passphrase = os.environ[RHO_PASSPHRASE]
         else:
             self.passphrase = getpass()
+
+        self.config = self._read_config(self.options.config, self.passphrase)
+        print(self.config)
 
         # do the work
         self._do_command()
@@ -195,8 +204,7 @@ class AuthAddCommand(CliCommand):
 
     def _validate_options(self):
         # file or username and password
-        if not self.options.filename or not self.options.username and not self.options.password:
-            print("option required")
+        pass
 
     def _do_command(self):
         if self.options.filename:
@@ -204,13 +212,13 @@ class AuthAddCommand(CliCommand):
             print("nothing needed")
         elif self.options.username and self.options.password:
             # using ssh
-            cred = SshCredentials({"name":self.options.name,
+            cred = config.SshCredentials({"name":self.options.name,
                 "username":self.options.username,
                 "password":self.options.password,
                 "type":"ssh"})
-            conf = Config(credentials=[cred])
-            print(conf.credentials)
-            print(conf.__dict__)
-            #f = open("rho.conf", 'w')
-            #f.write(json.dumps(conf.__dict__))
-            #f.close()
+            self.config.add_credentials(cred)
+            print(self.config.list_credentials())
+            c = config.ConfigBuilder().dump_config(self.config)
+            print(c)
+            print("[%s]" % self.passphrase)
+            crypto.write_file(self.options.config, c, self.passphrase)
