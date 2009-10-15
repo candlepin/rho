3a86f73338cec336d1672081323d5ab5a253e34f
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-10-15 14:44:19 -0400

    refactor of ssh_job
    SshJob now has a list of RhoCmds, and a prot number.
    
    SshJobs now refers to SshJobs as "ssh_jobs" instead of "cmds" which
    didnt make any sense anymore.

--- src/rho/ssh_jobs.py
+++ src/rho/ssh_jobs.py
@@ -24,16 +24,22 @@ class SshAuth(Auth):
         self.password = password
 
 
-
+#FIXME: SshJob needs to have a RhoJobsList, where each RhoJob item actually has
+# a list of cli commands to run
 class SshJob():
-    def __init__(self, ip=None, cmds=None, auth=None, timeout=30):
+    def __init__(self, ip=None, port=22, rho_cmds=None, auth=None, timeout=30):
         self.ip = ip
-        self.cmds = cmds
+        self.port = port
+
+        # rho commands is RhoCmdList, aka, a list of RhoCmds (duh)
+        self.rho_cmds = rho_cmds
+
         self.auth = auth
         self.timeout = timeout
         self.command_output = None
         self.connection_result = None
         self.returncode = None
+        self.auth_used = None
 
     def output_callback(self):
         print "ip: %s %s" % (self.ip, self.command_output)
@@ -43,51 +49,30 @@ class SshJob():
         # what is auth? undetermined yet
 #        self.auth = self.config['
 
+
 class SshJobs():
-    def __init__(self, cmdSrc=None):
-        self.read_set = []
-        self.write_set = []
-        self.jobs = []
-        self.jobs_read = []
-        self.jobs_write = []
+    def __init__(self, ssh_job_src=None):
         # cmdSrc is some sort of list/iterator thing
-        self.cmds_to_run = cmdSrc
+        self.ssh_jobs = ssh_job_src
 
         self.verbose = True
         self.outfile = None
         self.max_threads = 10  
 
-    def run_cmds(self, cmds=None, callback=None):
-        if cmds:
-            self.cmds_to_run = cmds
+    def run_jobs(self, jobs=None, callback=None):
+        if ssh_jobs:
+            self.ssh_jobs = jobs
         self.output_queue = my_sshpt.startOutputThread(self.verbose, self.outfile)
         self.ssh_connect_queue = my_sshpt.startSSHQueue(self.output_queue, self.max_threads)
 
-        while self.cmds_to_run:
-            for cmd in self.cmds_to_run:
+        while self.ssh_jobs:
+            for ssh_job in self.ssh_jobs:
                 if self.ssh_connect_queue.qsize()  <= self.max_threads:
-                    my_sshpt.queueSSHConnection(self.ssh_connect_queue, cmd)
-                    self.cmds_to_run.remove(cmd)
+                    my_sshpt.queueSSHConnection(self.ssh_connect_queue, ssh_job)
+                    self.ssh_jobs.remove(ssh_job)
 #            time.sleep(1)
         self.ssh_connect_queue.join()
         return self.output_queue
-
-    # this is a lame one at a time, single threaded, blocking, sync approah
-    # will replce with something better 
-    def run_cmds_old(self, callback=None):
-        while self.cmds_to_run:
-            job = self.cmds_to_run.pop()
-            job.run()
-            if callback:
-                callback([job])
-#            print job.output
-            self.jobs.append(job)
-
-    def add_cmd_to_run(self, job):
-        pass
-
-    def get_job(self):
-        pass
 
 
 def example_callback():
@@ -95,16 +80,16 @@ def example_callback():
         print "%s: %s" % (result.ip, result.output)
 
 if __name__ == "__main__":  
-    ssh_cmds = []
+    ssh_jobs = []
 
     auth = SshAuth(name="adrian", username="adrian")
 
     ip_range = ["alikins.usersys.redhat.com", "badhost.example.com"]
     for ip in ip_range:
-        ssh_cmds.append(SshJob(ip=ip, cmds=["echo foo bar %s" % ip], auth=auth ))
+        ssh_jobs.append(SshJob(ip=ip, rho_cmds=[rho_cmds.UnameRhoCmd()], auth=auth ))
         
     jobs = SshJobs()
-    jobs.cmds_to_run = ssh_cmds
-    jobs.run_cmds(callback=callback)
+#    jobs.cmds_to_run = ssh_cmds
+    jobs.run_cmds(ssh_jobs = ssh_jobs, callback=callback)
 
 #jobs.read_jobs()
