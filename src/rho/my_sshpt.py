70d8fcc4f1cfc108f2ead9fca8d0ab3879b7067b
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-10-15 14:40:42 -0400

    more refactoring of my_sshpt
    Pass around SshJob()'s whenever we can instead of a big list of args
    
    Change ParamikoConnect to use SshJob (and support changing the port)
    
    executeCommands gets a lot of change, it now gets a a list of RhoCmd's
    instead of a list of cmd strings.
    
    remove sshpt() method we no longer use

--- src/rho/my_sshpt.py
+++ src/rho/my_sshpt.py
@@ -18,14 +18,12 @@ class OutputThread(GenericThread):
     
         output_queue: Queue.Queue(): The queue to use for incoming messages.
         verbose - Boolean: Whether or not we should output to stdout.
-        outfile - String: Path to the file where we'll store results.
     """
-    def __init__(self, output_queue, verbose=True, outfile=None):
+    def __init__(self, output_queue, verbose=True):
         """Name ourselves and assign the variables we were instanciated with."""
         threading.Thread.__init__(self, name="OutputThread")
         self.output_queue = output_queue
         self.verbose = verbose
-        self.outfile = outfile
         self.quitting = False
 
     def run(self):
@@ -33,6 +31,8 @@ class OutputThread(GenericThread):
             queueObj = self.output_queue.get()
             if queueObj == "quit":
                 self.quit()
+
+            # somewhere in here, we return the data to...?
             self.output_queue.task_done()
 
 class SSHThread(GenericThread):
@@ -64,19 +64,15 @@ class SSHThread(GenericThread):
 #                if queueObj == 'quit':
 #                    self.quit()
                     
-                # These variable assignments are just here for readability further down
-                host = queueObj.ip
-                username = queueObj.auth.username
-                password = queueObj.auth.password
-                timeout = queueObj.timeout
-                commands = queueObj.cmds
-                
-                success, command_output = attemptConnection(host, username, password, timeout, commands)
-                if success:
+#                success, command_output = attemptConnection(host, username, password, timeout, commands)
+                attemptConnection(queueObj)
+
+                #hmm, this is weird...
+                if queueObj.connection_status:
                     queueObj.connection_result = "SUCCESS"
                 else:
                     queueObj.connection_result = "FAILED"
-                queueObj.command_output = command_output
+
                 self.output_queue.put(queueObj)
                 self.ssh_connect_queue.task_done()
                 # just for progress, etc...
@@ -122,92 +118,58 @@ def queueSSHConnection(ssh_connect_queue, cmd):
     ssh_connect_queue.put(cmd)
     return True
 
-def paramikoConnect(host, username, password, timeout):
+
+
+def paramikoConnect(ssh_job):
     """Connects to 'host' and returns a Paramiko transport object to use in further communications"""
     # Uncomment this line to turn on Paramiko debugging (good for troubleshooting why some servers report connection failures)
     #paramiko.util.log_to_file('paramiko.log')
     ssh = paramiko.SSHClient()
+
+    # FIXME: akl
+    # this is probably the place to try the different auth in order, and set some
+    # value on the ssh_job type so we can update config properly
     try:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, port=22, username=username, password=password, timeout=timeout)
+        ssh.connect(ssh_job.ip, port=ssh_job.port, 
+                    username=ssh_job.auth.username,
+                    password=ssh_job.auth.password, 
+                    timeout=ssh_job.timeout)
     except Exception, detail:
         # Connecting failed (for whatever reason)
         ssh = str(detail)
     return ssh
 
-def executeCommand(transport, command, password=None):
-    """Executes the given command via the specified Paramiko transport object.  Will execute as sudo if passed the necessary variables (sudo=True, password, run_as).
-    Returns stdout (after command execution)"""
+
+def excecuteCommands(tranport, rho_commands):
     host = transport.get_host_keys().keys()[0]
-    stdin, stdout, stderr = transport.exec_command(command)
-    command_output = stdout.readlines()
-    command_output = "".join(command_output)
-    return command_output
+    output = []
+    for rho_cmd in rho_commands:
+        for cmd_string in rho_cmd.cmd_strings:
+            stdin, stdout, stderr = transport.exec_command(cmd_string)
+            # one item in the list for each cmd stdout
+            output.append(stdout.readlines(), stderr.readlines())
+        rho_cmd.populate_data(output)
+    return rho_commands
 
-def attemptConnection(
-        host,
-        username,
-        password,
-        timeout=30, # Connection timeout
-        commands=False, # Either False for no commnads or a list
-        ):
-
-    connection_result = True
-    command_output = []
+def attemptConnection(ssh_job):
+    # ssh_job is a SshJob object
 
     if host != "":
         try:
-            ssh = paramikoConnect(host, username, password, timeout)
+            ssh = paramikoConnect(ssh_job)
             if type(ssh) == type(""): # If ssh is a string that means the connection failed and 'ssh' is the details as to why
-                connection_result = False
-                command_output = ssh
-                return connection_result, command_output
+                ssh_job.command_output = ssh
+                ssh_job.connection_result = False
             command_output = []
-            if commands:
-                for command in commands: # This makes a list of lists (each line of output in command_output is it's own item in the list)
-                    command_output.append(executeCommand(transport=ssh, command=command, password=password))
-            elif commands is False and execute is False: # If we're not given anything to execute run the uptime command to make sure that we can execute *something*
-                command_output = executeCommand(transport=ssh, command='uptime',  password=password)
-
+            executeCommands(transport=ssh, commands=ssh_job.rho_cmds)
             ssh.close()
-            command_count = 0
-            for output in command_output: # Clean up the command output
-                command_output[command_count] = output
-                command_count = command_count + 1
+
         except Exception, detail:
             # Connection failed
             #print "Exception: %s" % detail
-            connection_result = False
-            command_output = detail
+            
+            ssh_job.connection_result = False
+            ssh_job.command_output = detail
             ssh.close()
-        return connection_result, command_output
-
-def sshpt(
-        hostlist, # List - Hosts to connect to
-        username,
-        password,
-        max_threads=10, # Maximum number of simultaneous connection attempts
-        timeout=30, # Connection timeout
-        commands=False, # List - Commands to execute on hosts (if False nothing will be executed)
-        verbose=True, # Whether or not we should output connection results to stdout
-        outfile=None, # Path to the file where we want to store connection results
-        output_queue=None # Queue.Queue() where connection results should be put().  If none is given it will use the OutputThread default (output_queue)
-        ):
-
-    if output_queue is None:
-        output_queue = startOutputThread(verbose, outfile)
-    # Start up the Output and SSH threads
-    ssh_connect_queue = startSSHQueue(output_queue, max_threads)
-    
-    if not commands and not local_filepath: # Assume we're just doing a connection test
-        commands = ['echo CONNECTION TEST',]
-
-    while len(hostlist) != 0: # Only add items to the ssh_connect_queue if there are available threads to take them.
-        for host in hostlist:
-            if ssh_connect_queue.qsize() <= max_threads:
-                queueSSHConnection(ssh_connect_queue, host, username, password, timeout, commands)
-                hostlist.remove(host)
-        sleep(1)
-    ssh_connect_queue.join() # Wait until all jobs are done before exiting
-    return output_queue
 
