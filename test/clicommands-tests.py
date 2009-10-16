0e2e835630efc3ef0ba2a2483b68a89d9e6c8295
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-10-16 15:20:05 -0400

    add an  default "test" conf for testing commandline options.
    Check for sys.exit() on auth_add test (missing options throws
    a optparse.parse_error which calls sys.exit(), which is weird...)

--- test/clicommands-tests.py
+++ test/clicommands-tests.py
@@ -20,22 +20,25 @@ class CliCommandsTests(unittest.TestCase):
 
     def _run_test(self, cmd, args):
         os.environ[RHO_PASSPHRASE] = "blerg"
-
-        sys.argv = args
+        sys.argv = ["bin/rho" ]  + args + ["--config", "test/rho.conf.test"]
         cmd.main()
 
     def test_scan(self):
-        self._run_test(ScanCommand(), ["bin/rho", "scan"])
+        self._run_test(ScanCommand(), ["scan"])
 
     def test_profile_show(self):
-        self._run_test(ProfileShowCommand(), ["bin/rho", "profile", "show"])
+        self._run_test(ProfileShowCommand(), ["profile", "show"])
 
     def test_profile_add(self):
-        self._run_test(ProfileAddCommand(), ["bin/rho", "profile", "add", "--name", "profilename"])
+        self._run_test(ProfileAddCommand(), ["profile", "add", "--name", "profilename"])
 
 
     def test_auth_show(self):
-        self._run_test(AuthShowCommand(), ["bin/rho", "auth", "show"])
+        self._run_test(AuthShowCommand(), ["auth", "show"])
 
     def test_auth_add(self):
-        self._run_test(AuthAddCommand(), ["bin/rho", "auth", "add"])
+        try:
+            self._run_test(AuthAddCommand(), ["auth", "add"])
+        except SystemExit:
+            # we expect this to throw a optparse.parse.error
+            pass
