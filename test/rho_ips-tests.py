56f75c336bc9389d439e459a61a4a9effca0116c
Adrian Likins <adrian@alikins.usersys.redhat.com>
2009-10-20 11:52:37 -0400

    add some test cases for rho_ips.py

---
+++ test/rho_ips-tests.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import unittest
+
+from rho import rho_ips
+
+
+class TestRhoIps(unittest.TestCase):
+    def setUp(self):
+        pass
+#        self.ipr = rho_ips.RhoIpRange(self.iprange)
+
+    # just for debugging results
+    def _cmp_list(self, x_list, y_list):
+        in_x_not_in_y = []
+        in_y_not_in_x = []
+        for x in x_list:
+            if x not in y_list:
+                in_x_not_in_y.append(x)
+
+        for y in y_list:
+            if y not in x_list:
+                in_y_not_in_x.append(y)
+
+        print "in_expected not in results: ", in_x_not_in_y
+        print "in_ results not in expected: ", in_y_not_in_x
+
+    def _check_ipr(self, iprange, expected):
+        self.ipr = rho_ips.RhoIpRange(iprange)
+        list_of_ips= list(self.ipr.ips)
+        ips = map(str, list_of_ips)
+#        list_of_ips = self.ipr.list_ips()
+        ips.sort()
+    
+        expected.sort()
+#        print len(ips), len(expected)
+#        print ips[0],ips[-1]
+#        print expected[0], expected[-1]
+#        self._cmp_list(expected, ips)
+#        print  ips, expected
+        assert ips == expected
+
+    def testIp(self):
+        self._check_ipr("10.0.0.1", ["10.0.0.1"])
+
+    def testLocalhost(self):
+        self._check_ipr("localhost", ["127.0.0.1"])
+
+    def testWildcard(self):
+        expected = []
+        for i in range(0,256):
+            expected.append("10.0.0.%s" % i)
+        self._check_ipr("10.0.0.*", expected)
+
+    def testIpRange(self):
+        
+        expected = []
+        for i in range(1,6):
+            expected.append("10.0.0.%s" % i)
+        self._check_ipr("10.0.0.1 - 10.0.0.5", expected)
+
+    def testCIDR_24(self):
+        expected = []
+        for i in range(0,256):
+            expected.append("10.0.0.%s" % i)
+        self._check_ipr("10.0.0.0/24", expected)
+        
+
+
+    def testIpRangeLarge(self):
+        expected = []
+        for i in range(0,4):
+            for j in range(0,256):
+                expected.append("10.0.%s.%s" % (i, j))
+        self._check_ipr("10.0.0.0 - 10.0.3.255", expected)
+
+    
