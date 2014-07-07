a8b7cde1927f68372f7e84b425660488435c5215
cnsnyder <csnyder@redhat.com>
2014-07-07 16:29:45 -0400

    Adds pack-scan.sh [USERNAME] [/path/to/file_of_hosts]

---
+++ pack-scan.sh
@@ -0,0 +1,3 @@
+
+rho scan --username $1 --hosts $2 --allow-agent --output pack-scan.csv --report-format misc.date,uname.hostname,redhat-release.release,redhat-packages.is_redhat,redhat-packages.ratio,redhat-packages.last_installed,redhat-packages.last_built,virt.virt,virt.type,cpu.count,cpu.socket_count
+
