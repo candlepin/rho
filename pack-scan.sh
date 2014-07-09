e1555a73c0ccfd900bb74df1f78438d22138fd2c
cnsnyder <csnyder@redhat.com>
2014-07-09 14:10:19 -0400

    Adds virt.num_guests field to pack-scan report script.

--- pack-scan.sh
+++ pack-scan.sh
@@ -1,3 +1,3 @@
 
-rho scan --username $1 --hosts $2 --allow-agent --output pack-scan.csv --report-format misc.date,uname.hostname,redhat-release.release,redhat-packages.is_redhat,redhat-packages.ratio,redhat-packages.last_installed,redhat-packages.last_built,virt.virt,virt.type,cpu.count,cpu.socket_count
+rho scan --username $1 --hosts $2 --allow-agent --output pack-scan.csv --report-format misc.date,uname.hostname,redhat-release.release,redhat-packages.is_redhat,redhat-packages.ratio,redhat-packages.last_installed,redhat-packages.last_built,virt.virt,virt.type,virt.num_guests,cpu.count,cpu.socket_count
 
