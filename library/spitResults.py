dae58b11118ba8933cb9dc5b0086780607b50104
Karthik Harihar Reddy Battula <kbattula@localhost.localdomain>
2016-06-09 12:59:32 -0400

    writes all default information to a csv

--- library/spitResults.py
+++ library/spitResults.py
@@ -1,27 +1,31 @@
 import csv
+import ast
 
 
 class Results(object):
 
-    def __init__(self,module):
+    def __init__(self, module):
         self.module = module
         self.vals = module.params['vals']
+        self.token = module.params['token']
 
     def writeToCSV(self):
-        f = open("report", "wb")
-        vals = self.vals
-        print vals
-        fields = vals[vals.keys()[0]].keys()
+        f = open("report.csv", "a")
+        vals = ast.literal_eval(self.vals)
+        token = ast.literal_eval(self.token)
+        fields = vals.keys()
         writer = csv.DictWriter(f, fieldnames=fields)
-        writer.writeheader()
-        for k in vals.keys():
-            writer.write(vals[k])
+        if token == 0:
+            writer.writeheader()
+            return
+        writer.writerow(vals)
 
 
 from ansible.module_utils.basic import *
 
 def main():
-    module = AnsibleModule(argument_spec=dict(name=dict(required=True),vals = dict(required = True)))
+    module = AnsibleModule(argument_spec=dict(name=dict(required=True), vals=dict(required=True),
+                                              token=dict(required=True)))
     results = Results(module=module)
     results.writeToCSV()
     response = {"written": "yes"}
